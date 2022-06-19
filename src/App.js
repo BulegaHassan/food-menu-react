3d0aa089d7104d560a8bbf3fd0ee6585fac286d3
Hassan Bulega <hassanbulega@gmail.com>
2022-06-19 12:15:05 +0300

    feat(component): add functionality to show all menu items or  by categor using buttonsy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
 
 function App() {
-  const[menuItems,setMenuItems]=useState(items)
-  const [categories,setCategories]=useState([])
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
 
-  return(
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems)
+  };
+
+  return (
     <main>
-      <section className="menu section">
-        <div className="title">
+      <section className='menu section'>
+        <div className='title'>
           <h2>our menu</h2>
-          <div className="underline"></div>
+          <div className='underline'></div>
         </div>
-        <Categories/>
-        <Menu items={menuItems}/>
+        <Categories filterItems={filterItems} />
+        <Menu items={menuItems} />
       </section>
     </main>
-  )
+  );
 }
 
 export default App;
